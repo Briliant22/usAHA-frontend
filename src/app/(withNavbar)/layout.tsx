a199cfeed3a187be778f6808c2e1702d16df9141
briliant22 <briliantnajmi@gmail.com>
2024-08-02 22:13:57 +0700

    bismillah sekali lagi

--- src/app/(withNavbar)/layout.tsx
+++ src/app/(withNavbar)/layout.tsx
@@ -1,7 +1,8 @@
+import LoadingPage from "@/components/loadingPage";
 import Navbar from "@/components/navbar/navbar";
 import NavbarAtas from "@/components/navbar/navbarAtas";
 
-import React from "react";
+import React, { Suspense } from "react";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -9,14 +10,16 @@ interface LayoutProps {
 
 export default async function Layout({ children }: LayoutProps) {
   return (
-    <div>
-      <div className="relative flex">
-        <Navbar />
-        <div className="flex h-screen w-full flex-col px-20 py-9">
-          <NavbarAtas isDashboard={true}/>
-          <div className="flex-grow overflow-y-auto">{children}</div>
+    <Suspense fallback={<LoadingPage />}>
+      <div>
+        <div className="relative flex">
+          <Navbar />
+          <div className="flex h-screen w-full flex-col px-20 py-9">
+            <NavbarAtas isDashboard={true} />
+            <div className="flex-grow overflow-y-auto">{children}</div>
+          </div>
         </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
